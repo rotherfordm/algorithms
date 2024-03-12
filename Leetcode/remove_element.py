777e71b7b5befdeb002b08395d1799d75cf2c1fd
Rotherford <rmarmibe@protonmail.com>
2024-03-12 15:58:14 +0800

    Update remove_element.py

--- Leetcode/remove_element.py
+++ Leetcode/remove_element.py
@@ -1,16 +1,10 @@
 def removeElement(nums, val):
-    replaced = 0
     k = 0
-    for i, num in enumerate(nums):
-        if num == val:
-            nums[i] = "_"
-            replaced += 1
-        else:
+    for i in range(len(nums)):
+        if nums[i] != val:
+            # partition
+            nums[k] = nums[i]
             k += 1
-
-    nums = [i for i in nums if i != "_"] + ["_" for i in range(0, replaced)]
-
-    print(k, nums, replaced)
     return k
 
 
